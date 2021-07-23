b4bd424e1bebfc759b659871683457e8c148f5e6
Elvin <camalzade_elvin@mail.ru>
2021-07-23 10:40:06 +0400

    done

--- WinformLesson3/Form1.Designer.cs
+++ WinformLesson3/Form1.Designer.cs
@@ -30,6 +30,10 @@
         {
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.carTxtb = new System.Windows.Forms.TextBox();
+            this.addBtn = new System.Windows.Forms.Button();
+            this.sendAllBtn = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // listBox1
@@ -48,17 +52,63 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 48F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(432, 55);
+            this.label1.Location = new System.Drawing.Point(500, 316);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(174, 79);
             this.label1.TabIndex = 1;
             this.label1.Text = "label1";
+            // 
+            // carTxtb
+            // 
+            this.carTxtb.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.carTxtb.Location = new System.Drawing.Point(56, 316);
+            this.carTxtb.Name = "carTxtb";
+            this.carTxtb.Size = new System.Drawing.Size(184, 31);
+            this.carTxtb.TabIndex = 2;
+            // 
+            // addBtn
+            // 
+            this.addBtn.AutoSize = true;
+            this.addBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.addBtn.Location = new System.Drawing.Point(247, 316);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(76, 35);
+            this.addBtn.TabIndex = 3;
+            this.addBtn.Text = "Add";
+            this.addBtn.UseVisualStyleBackColor = true;
+            this.addBtn.Click += new System.EventHandler(this.addBtn_Click);
+            // 
+            // sendAllBtn
+            // 
+            this.sendAllBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.sendAllBtn.Location = new System.Drawing.Point(56, 366);
+            this.sendAllBtn.Name = "sendAllBtn";
+            this.sendAllBtn.Size = new System.Drawing.Size(267, 31);
+            this.sendAllBtn.TabIndex = 4;
+            this.sendAllBtn.Text = "Send All";
+            this.sendAllBtn.UseVisualStyleBackColor = true;
+            this.sendAllBtn.Click += new System.EventHandler(this.sendAllBtn_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox2.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 25;
+            this.listBox2.Location = new System.Drawing.Point(383, 28);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(267, 254);
+            this.listBox2.TabIndex = 5;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.sendAllBtn);
+            this.Controls.Add(this.addBtn);
+            this.Controls.Add(this.carTxtb);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.listBox1);
             this.Name = "Form1";
@@ -73,6 +123,10 @@
 
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox carTxtb;
+        private System.Windows.Forms.Button addBtn;
+        private System.Windows.Forms.Button sendAllBtn;
+        private System.Windows.Forms.ListBox listBox2;
     }
 }
 
--- WinformLesson3/Form1.cs
+++ WinformLesson3/Form1.cs
@@ -30,28 +30,40 @@ namespace WinformLesson3
             //listBox1.SelectedIndex = 2;
 
 
-            List<Car> cars = new List<Car>
-            {
-                new Car
-                {
-                    Name="Mercedes"
-                },
-                new Car
-                {
-                    Name="Audi"
-                },
-                new Car
-                {
-                    Name="Ferrari"
-                },
-                new Car
-                {
-                    Name="Cruze 2LT"
-                },
-            };
+            //List<Car> cars = new List<Car>
+            //{
+            //    new Car
+            //    {
+            //        Name="Mercedes"
+            //    },
+            //    new Car
+            //    {
+            //        Name="Audi"
+            //    },
+            //    new Car
+            //    {
+            //        Name="Ferrari"
+            //    },
+            //    new Car
+            //    {
+            //        Name="Cruze 2LT"
+            //    },
+            //};
 
-            listBox1.DataSource = cars;
-            listBox1.DisplayMember = "Id";
+            //listBox1.DataSource = cars;
+
+            listBox1.Items.Add(new Car
+            {
+                Name = "Mercedes"
+            });
+            listBox1.Items.Add(new Car
+            {
+                Name="Ferrari"
+            });
+
+            listBox1.DisplayMember = "Name";
+
+            //listBox1.SelectionMode = SelectionMode.MultiSimple;
 
         }
 
@@ -59,6 +71,42 @@ namespace WinformLesson3
         {
             //var item = listBox1.SelectedItem as string;
             //label1.Text = item;
+            //var item = listBox1.SelectedItem as Car;
+            //label1.Text = item?.Name;
+
+            var obj = listBox1.SelectedItem;
+            try
+            {
+
+            listBox1.Items.Remove(obj);
+            listBox2.Items.Add(obj);
+            listBox1.SelectedItem = null;
+            }
+            catch (Exception)
+            {
+
+            }
+
+        }
+
+        private void addBtn_Click(object sender, EventArgs e)
+        {
+            //listBox1.DisplayMember = "Id";
+            listBox1.Items.Add(new Car
+            {
+                Name = carTxtb.Text
+            });
+        }
+
+        private void sendAllBtn_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in listBox1.SelectedItems)
+            {
+                var car = item as Car;
+                sb.Append($"{car?.Name}\n");
+            }
+            label1.Text = sb.ToString();
         }
     }
 }
